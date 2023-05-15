6f76105c46ed5fb77007b3b57f0149f2cdcbcd6e
bilguun111101 <Mason@Masons-MacBook-Pro.local>
2023-05-15 23:44:21 +0800

    Making Post section

---
+++ src/components/posts/PostFeed.tsx
@@ -0,0 +1,21 @@
+import usePosts from "@/hooks/usePosts";
+import { memo } from "react";
+
+import PostItem from "./PostItem";
+
+interface PostFeedProps {
+  userId?: string;
+}
+
+const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
+  const { data: posts = [] } = usePosts(userId as string);
+  return (
+    <>
+      {posts.map((post: Record<string, any>) => (
+        <PostItem userId={userId} key={post.id} data={post} />
+      ))}
+    </>
+  );
+};
+
+export default memo(PostFeed);
